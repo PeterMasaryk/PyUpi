567cf21707dbd7b04acb20ffdde70c6795f69828
PeterMasaryk <masaryk.peter00@gmail.com>
2024-12-05 23:59:31 +0100

    Migration Add star sign column to UserModel

--- app/models/UserModel.py
+++ app/models/UserModel.py
@@ -4,11 +4,13 @@ class UserModel(SQLModel, table=True):
 
     __tablename__ = "users"
 
-    def __init__(self, id, username, email):
+    def __init__(self, id, username, email, star_sign):
         self.id = id
         self.username = username
         self.email = email
+        self.star_sign = star_sign
 
     id: str = Field(primary_key=True)
     username: str = Field(index=True)
     email: str | None = Field(default=None, index=True)
+    star_sign: str
